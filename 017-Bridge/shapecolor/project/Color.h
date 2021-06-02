6e49abbed16a3d2002c5663cd12556e63515b090
Dinçer Ünal <dincer.unal0652@gmail.com>
2021-06-02 14:05:20 +0300

    Bridge
    shapecolor, personimplvoidpointer, PersonImpl

---
+++ 017-Bridge/shapecolor/project/Color.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+class Color {
+public:
+	virtual void FillColor() = 0;
+};
---
+++ 017-Bridge/shapecolor/project/RedColor.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Color.h"
+
+class RedColor : public Color {
+public:
+	void FillColor() override;
+};
+
